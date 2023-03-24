c418df0d0813a1a57242650419b6b41995147f65
Aarthi-Nunna <84324124+Aarthi-Nunna@users.noreply.github.com>
2023-03-24 11:20:25 +0530

    update

--- cids/chaincode-rest-api/src/server.ts
+++ cids/chaincode-rest-api/src/server.ts
@@ -6,11 +6,16 @@
 
 import express, { Request, Response } from 'express';
 import { modelUpdateRouter } from './model-update.router.js';
+import { modelUpdateRouterUI } from './model-update-ui.router.js';
 import { infoln } from './utils.js';
 
 export async function startServer(): Promise<void> {
     const server = express();
     const port = 3000;
+
+    server.set("view engine","ejs");
+    //server.set('views', './views');
+
 
     server.use(express.json());
     server.use(express.urlencoded({
@@ -22,6 +27,7 @@ export async function startServer(): Promise<void> {
     });
 
     server.use('/api/model', modelUpdateRouter);
+    server.use('/api/intrusion',modelUpdateRouterUI);
 
     server.listen(port, () => {
         infoln(`\nExample app listening on port ${port}`);
